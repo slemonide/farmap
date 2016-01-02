fa2e3191fe72930dc7429b3b7a35d117bc2a9e16
Lemon Slemon <slemonide@gmail.com>
2016-01-02 09:45:36 -0800

    No more lags

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+-- New tactics: update skymap only when player is not moving
+
 modpath = minetest.get_modpath("farmap")
 dofile(modpath .. "/textures.lua") -- Load textures
 skybox_data = {} -- Skybox related data
@@ -7,9 +9,9 @@ function draw_skybox_tile(dir, pos)
 	--pos = {x=0,y=300,z=0} -- for debug
 	local D = 0
 	if minetest.get_modpath("gridgen") then
-		D = 20
+		D = 24
 	else
-		D = 5
+		D = 24
 	end
 
 	local tiles = "[combine:" .. 2*D + 1 .. "x" .. 2*D + 1
@@ -66,62 +68,50 @@ function draw_skybox_tile(dir, pos)
 end
 
 function set_skybox()
+
+--	local t1 = os.clock()
+--	local geninfo = "[farmap] drawing..."
+--	minetest.chat_send_all(geninfo)
+
 	for _,player in ipairs(minetest.get_connected_players()) do
-		local pos = vector.round(player:getpos())
 		local player_name = player:get_player_name()
 		if not skybox_data[player_name] then
 			skybox_data[player_name] = {}
 		end
 
-		if not skybox_data[player_name].sky_count then
-			skybox_data[player_name].sky_count = 1
+		local pos = player:getpos()
+
+		local map_done = false
+		if not skybox_data[player_name].old_pos then
+			skybox_data[player_name].old_pos = pos
+			break
+		elseif vector.equals(skybox_data[player_name].old_pos, pos) then
+			if map_done then
+				break
+			else
+				map_done = true
+			end
+		else
+			map_done = false
+			break
 		end
 
-		if skybox_data[player_name].sky_count >= 4 then
-			skybox_data[player_name].sky_count = 1
-		elseif skybox_data[player_name].sky_count < 4 then
-			skybox_data[player_name].sky_count = skybox_data[player_name].sky_count + 1
+		local dirs = {"px", "py", "pz", "nx", "ny", "nz"}
+		--local dirs = {"px", "pz", "nx", "nz"}
+		local dir = {}
+
+		for _,s_dir in pairs(dirs) do -- Prepare dir table
+			dir[s_dir] = draw_skybox_tile(s_dir, pos)
 		end
 
-		--local dirs = {"px", "py", "pz", "nx", "ny", "nz"}
-		local dirs = {"px", "nx", "nz", "pz"}
-
-		local dir = draw_skybox_tile(dirs[skybox_data[player_name].sky_count], pos)
-
-		skybox_data[player_name].sky[2 + skybox_data[player_name].sky_count] = dir
-
-		player:set_sky({}, "skybox", skybox_data[player_name].sky)
-
-	end
-	minetest.after(1, set_skybox)
-end
-minetest.after(1, set_skybox)
-
-minetest.after(0, function()
-minetest.register_on_joinplayer(function(player)
-	local player_name = player:get_player_name()
-	if not skybox_data[player_name] then
-		skybox_data[player_name] = {}
-	end
-
-	local pos = vector.round(player:getpos())
-
-	--local dirs = {"px", "py", "pz", "nx", "ny", "nz"}
-	local dirs = {"px", "pz", "nx", "nz"}
-	local dir = {}
-
-	for _,s_dir in pairs(dirs) do -- Prepare dir table
-		dir[s_dir] = draw_skybox_tile(s_dir, pos)
-	end
-
-	local skytextures = {
-		textures["air"],
-		textures["default:dirt"],
-		dir.px,
-		dir.nx,
-		dir.nz,
-		dir.pz,
-	}
+		local skytextures = {
+			dir.py,
+			dir.ny,
+			dir.px,
+			dir.nx,
+			dir.nz,
+			dir.pz,
+		}
 
 --[[
 	local skytextures = {
@@ -135,7 +125,19 @@ minetest.register_on_joinplayer(function(player)
 		dir.pz .. "^[transformFY",
 	}
 --]]
-	skybox_data[player_name].sky = skytextures
-	player:set_sky({}, "skybox", skytextures)
-	end)
+		skybox_data[player_name].sky = skytextures
+		player:set_sky({}, "skybox", skytextures)
+	end
+
+--	local t2 = os.clock()
+--	local calcdelay = string.format("%.2fs", t2 - t1)
+--	local geninfo = "[farmap] done after ca.: " .. calcdelay
+--	minetest.chat_send_all(geninfo)
+
+	minetest.after(1, set_skybox)
+end
+minetest.after(1, set_skybox)
+
+minetest.register_on_joinplayer(function(player)
+
 end)
